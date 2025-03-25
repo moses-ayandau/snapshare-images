04d4fde38cd24b392dcc0f5cca54f5c0cf19eb71
mwayandau1 <mosesayandau1@gmail.com>
2025-03-25 21:14:29 +0000

    change of database

--- src/main/java/com/moses/fun/config/DataSourceConfig.java
+++ src/main/java/com/moses/fun/config/DataSourceConfig.java
@@ -21,16 +21,16 @@ public class DataSourceConfig {
         System.out.println("password "+parameterStoreService.getParameter("/snapshare/db/password") );
 
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//        dataSource.setDriverClassName("org.postgresql.Driver");
-//        dataSource.setUrl(parameterStoreService.getParameter("/snapshare/db/url"));
-//        dataSource.setUsername(parameterStoreService.getParameter("/snapshare/db/username"));
-//        dataSource.setPassword(parameterStoreService.getParameter("/snapshare/db/password"));
-
-
         dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl(parameterStoreService.getParameter("/test/db/snapshare/url"));
-        dataSource.setUsername(parameterStoreService.getParameter("/test/db/snapshare/username"));
-        dataSource.setPassword(parameterStoreService.getParameter("/test/db/snapshare/password"));
+        dataSource.setUrl(parameterStoreService.getParameter("/snapshare/db/url"));
+        dataSource.setUsername(parameterStoreService.getParameter("/snapshare/db/username"));
+        dataSource.setPassword(parameterStoreService.getParameter("/snapshare/db/password"));
+
+
+//        dataSource.setDriverClassName("org.postgresql.Driver");
+//        dataSource.setUrl(parameterStoreService.getParameter("/test/db/snapshare/url"));
+//        dataSource.setUsername(parameterStoreService.getParameter("/test/db/snapshare/username"));
+//        dataSource.setPassword(parameterStoreService.getParameter("/test/db/snapshare/password"));
         return dataSource;
     }
 }
