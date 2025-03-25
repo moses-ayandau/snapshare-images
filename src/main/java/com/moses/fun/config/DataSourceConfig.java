92e1cd4410d55bcc31e7c0634a05a9da39822111
mwayandau1 <mosesayandau1@gmail.com>
2025-03-25 16:34:56 +0000

    git actions template

--- src/main/java/com/moses/fun/config/DataSourceConfig.java
+++ src/main/java/com/moses/fun/config/DataSourceConfig.java
@@ -22,9 +22,15 @@ public class DataSourceConfig {
 
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl(parameterStoreService.getParameter("/test/db/snapshare/url"));
-        dataSource.setUsername(parameterStoreService.getParameter("/test/db/snapshare/username"));
-        dataSource.setPassword(parameterStoreService.getParameter("/test/db/snapshare/password"));
+        dataSource.setUrl(parameterStoreService.getParameter("/snapshare/db/url"));
+        dataSource.setUsername(parameterStoreService.getParameter("/snapshare/db/username"));
+        dataSource.setPassword(parameterStoreService.getParameter("/snapshare/db/password"));
+
+
+//        dataSource.setDriverClassName("org.postgresql.Driver");
+//        dataSource.setUrl(parameterStoreService.getParameter("/test/db/snapshare/url"));
+//        dataSource.setUsername(parameterStoreService.getParameter("/test/db/snapshare/username"));
+//        dataSource.setPassword(parameterStoreService.getParameter("/snapshare/db/password"));
         return dataSource;
     }
 }
