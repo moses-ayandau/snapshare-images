75fda934e6a4b7c3883205dd9704b76c54f1adf3
mwayandau1 <mosesayandau1@gmail.com>
2025-03-27 08:15:31 +0000

    modified

--- src/main/java/com/moses/fun/config/DataSourceConfig.java
+++ src/main/java/com/moses/fun/config/DataSourceConfig.java
@@ -1,36 +1,41 @@
-package com.moses.fun.config;
-
-import com.moses.fun.service.ParameterStoreService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
-import javax.sql.DataSource;
-
-@Configuration
-public class DataSourceConfig {
-
-    @Autowired
-    private ParameterStoreService parameterStoreService;
-
-    @Bean
-    public DataSource dataSource() {
-        System.out.println("url "+parameterStoreService.getParameter("/snapshare/db/url") );
-        System.out.println("username "+parameterStoreService.getParameter("/snapshare/db/username") );
-        System.out.println("password "+parameterStoreService.getParameter("/snapshare/db/password") );
-
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl(parameterStoreService.getParameter("/snapshare/db/url"));
-        dataSource.setUsername(parameterStoreService.getParameter("/snapshare/db/username"));
-        dataSource.setPassword(parameterStoreService.getParameter("/snapshare/db/password"));
-
-
+//package com.moses.fun.config;
+//
+//import com.moses.fun.service.ParameterStoreService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.jdbc.datasource.DriverManagerDataSource;
+//
+//import javax.sql.DataSource;
+//
+//@Configuration
+//public class DataSourceConfig {
+//
+//    @Autowired
+//    private ParameterStoreService parameterStoreService;
+//
+//    @Bean
+//    public DataSource dataSource() {
+////        System.out.println("url "+parameterStoreService.getParameter("/snapshare/db/url") );
+////        System.out.println("username "+parameterStoreService.getParameter("/snapshare/db/username") );
+////        System.out.println("password "+parameterStoreService.getParameter("/snapshare/db/password") );
+//
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
 //        dataSource.setDriverClassName("org.postgresql.Driver");
-//        dataSource.setUrl(parameterStoreService.getParameter("/test/db/snapshare/url"));
-//        dataSource.setUsername(parameterStoreService.getParameter("/test/db/snapshare/username"));
-//        dataSource.setPassword(parameterStoreService.getParameter("/test/db/snapshare/password"));
-        return dataSource;
-    }
-}
+////        dataSource.setUrl(parameterStoreService.getParameter("/snapshare/db/url"));
+////        dataSource.setUsername(parameterStoreService.getParameter("/snapshare/db/username"));
+////        dataSource.setPassword(parameterStoreService.getParameter("/snapshare/db/password"));
+//
+//        dataSource.setUrl("jdbc:postgresql://database-1.cra46k0ggz73.eu-central-1.rds.amazonaws.com:5432/testdb");
+//        dataSource.setUsername("postgres");
+//        dataSource.setPassword("Mos21311");
+//
+//
+//
+////        dataSource.setDriverClassName("org.postgresql.Driver");
+////        dataSource.setUrl(parameterStoreService.getParameter("/test/db/snapshare/url"));
+////        dataSource.setUsername(parameterStoreService.getParameter("/test/db/snapshare/username"));
+////        dataSource.setPassword(parameterStoreService.getParameter("/test/db/snapshare/password"));
+//        return dataSource;
+//    }
+//}
--- src/main/java/com/moses/fun/service/ParameterStoreService.java
+++ src/main/java/com/moses/fun/service/ParameterStoreService.java
@@ -1,28 +1,28 @@
-package com.moses.fun.service;
-
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.ssm.SsmClient;
-import software.amazon.awssdk.services.ssm.model.GetParameterRequest;
-import org.springframework.stereotype.Service;
-
-@Service
-public class ParameterStoreService {
-    private final SsmClient ssmClient;
-
-    public ParameterStoreService() {
-        this.ssmClient = SsmClient.builder()
-                .region(Region.EU_CENTRAL_1)
-                .credentialsProvider(DefaultCredentialsProvider.create())
-                .build();
-    }
-
-    public String getParameter(String name) {
-        GetParameterRequest request = GetParameterRequest.builder()
-                .name(name)
-                .withDecryption(true)
-                .build();
-
-        return ssmClient.getParameter(request).parameter().value();
-    }
-}
+//package com.moses.fun.service;
+//
+//import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+//import software.amazon.awssdk.regions.Region;
+//import software.amazon.awssdk.services.ssm.SsmClient;
+//import software.amazon.awssdk.services.ssm.model.GetParameterRequest;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class ParameterStoreService {
+//    private final SsmClient ssmClient;
+//
+//    public ParameterStoreService() {
+//        this.ssmClient = SsmClient.builder()
+//                .region(Region.EU_CENTRAL_1)
+//                .credentialsProvider(DefaultCredentialsProvider.create())
+//                .build();
+//    }
+//
+//    public String getParameter(String name) {
+//        GetParameterRequest request = GetParameterRequest.builder()
+//                .name(name)
+//                .withDecryption(true)
+//                .build();
+//
+//        return ssmClient.getParameter(request).parameter().value();
+//    }
+//}
